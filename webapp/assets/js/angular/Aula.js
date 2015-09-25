feae232d730d04d216f1e90f16ee1b617df424a4
Corella <alecg091293@gmail.com>
2015-09-24 22:00:32 -0600

    Persistencia de datos en session lista, funcion de buscar aula terminada

--- webapp/assets/js/angular/Aula.js
+++ webapp/assets/js/angular/Aula.js
@@ -1,15 +1,25 @@
-app.controller('AulaCtrl',['$scope', function($scope){
-        $scope.direccionSeleccionada = '';
-        $scope.aulaSeleccionada = '';
-        
-        $scope.buscarAula = function(aulaId){
-           for (var i = 0; i = aulasDisponibles.length; i++) 
-                if(aulasDisponibles[i].id === aulaId)
-                    $scope.direccionSeleccionada = aulasDisponibles[i].direccion;
+app.controller('CtrlAu',['$scope', function($scope){
+     $scope.campus = 'Undefined';
+	 console.log("Message " + sessionStorage.getItem('campus'));
+	 $scope.campus = sessionStorage.getItem('campus');
+	 $scope.direccionSeleccionada = '';
+     $scope.aulaSeleccionada = '';
+     $scope.aulasDisponibles = [
+		 //Lista de Aulas
+             { id: '321', direccion: 'Filosofia y letras, 3er piso'},
+             { id: '221', direccion: ''}
+        ]; 
+	
+        $scope.buscarAula = function(){
+		   var temp;
+           for (temp of $scope.aulasDisponibles) {
+    			if(temp.id === $scope.aulaSeleccionada)
+                    $scope.direccionSeleccionada = temp.direccion;
+		   	}
+			if($scope.direccionSeleccionada === '')
+				$scope.direccionSeleccionada = 'Aula no encontrada';
         };
         
-        $scope.aulasDisponibles = [
-             { id: '', direccion: ''},
-             { id: '', direccion: ''},
-        ];
 }]);
+
+
--- webapp/assets/js/angular/InicioCtrl.js
+++ webapp/assets/js/angular/InicioCtrl.js
@@ -2,11 +2,11 @@ app.controller('InicioCtrl',['$scope', function($scope){
         $scope.campus = 'Seleccione el campus';
         $scope.campusCodigoSeleccionado = '';
         
-        $scope.seleccionarCampus = function(index){
-           $scope.campus = campusDisponibles[index];
-           $scope.campusCodigoSeleccionado = campusDisponibles[index].id;
-        };
-        
+    	$scope.$watch("campus", function() {
+     		sessionStorage.setItem('campus',$scope.campus);
+		 	console.log("Message " + sessionStorage.getItem('campus'));
+  		});
+    
         $scope.campusDisponibles = [
             {
                 nombre: 'Benjamin Nunez',
